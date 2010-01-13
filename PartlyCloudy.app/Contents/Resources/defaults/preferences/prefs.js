86e64a56412ba75bef4def8636b9dc96ce15edaa
Jake Dahn <admin@jakedahn.com>
2010-01-12 23:48:18 -0600

    adding js debug stuff

--- PartlyCloudy.app/Contents/Resources/defaults/preferences/prefs.js
+++ PartlyCloudy.app/Contents/Resources/defaults/preferences/prefs.js
@@ -1,1 +1,10 @@
 pref("toolkit.defaultChromeURI", "chrome://partlycloudy/content/main.xul");
+
+
+// debug prefs
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+
